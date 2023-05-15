975c7c6c6b2a9a46a07320ce027e3a2735370756
whyevenquestion1t <shoysoronovr@gmail.com>
2023-05-14 22:50:52 -0400

    changed-ecs-for-ci/cd

--- terraform/04_ecs.tf
+++ terraform/04_ecs.tf
@@ -1,23 +1,23 @@
 resource "aws_ecs_cluster" "production" {
-  name = "terraform-ecs-cluster"
+  name = "ecs-demo"
 }
 
 resource "aws_ecs_task_definition" "task_1" {
-  family                   = "terraform"
+  family                   = "ecs-demo"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
   memory                   = 2048
   container_definitions = jsonencode([
     {
-      name  = "nginx-terraform"
-      image = "public.ecr.aws/nginx/nginx:perl"
+      name  = "web"
+      image = "registry.gitlab.com/shoysoronovr/eco-demo/master:latest"
 
       essential = true
       portMappings = [
         {
-          containerPort = 80
-          hostPort      = 80
+          containerPort = 5000
+          hostPort      = 5000
         }
       ]
     }
@@ -25,10 +25,10 @@ resource "aws_ecs_task_definition" "task_1" {
 }
 
 resource "aws_ecs_service" "production" {
-  name            = "demo-service-terraform"
+  name            = "ecs-demo"
   cluster         = aws_ecs_cluster.production.id
   task_definition = aws_ecs_task_definition.task_1.arn
-  desired_count   = 3
+  desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
     subnets         = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id]
@@ -36,7 +36,7 @@ resource "aws_ecs_service" "production" {
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.tg.arn
-    container_name   = "nginx-terraform"
-    container_port   = 80
+    container_name   = "web"
+    container_port   = 5000
   }
 }
